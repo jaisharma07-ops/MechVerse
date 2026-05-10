295dc09ca1fe8f9511562ead1da2c74b70bc85a7
angarkartanmay-ops <angarkartanmay@gmail.com>
2026-05-10 08:58:11 +0530

    chore: remove old landing page, clean slate for redesign

--- machineverse/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Splash from "@/components/Splash";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background text-text-primary">
-      <Splash />
-    </main>
-  );
-}
